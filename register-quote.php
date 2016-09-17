f33734c4cef5a6f930eb9b877ad7ee68cc8344fb
Hari Shanker <hari.shanker@automattic.com>
2016-09-17 16:09:01 -0700

    Sanitized Functions

--- register-quote.php
+++ register-quote.php
@@ -1,22 +1,22 @@
 <?php
-add_action( 'init', 'create_post_type' );
-function create_post_type() {
+add_action( 'init', 'qtd_create_post_type' );
+function qtd_create_post_type() {
   register_post_type( 'quote_display',
     array(
       'labels' => array(
         'name' => __( 'Quotes' ),
         'singular_name' => __( 'Quote' ),
-        'add_new' => 'Add New Quote',
-        'add_new_item' => 'Add New Quote',
-        'edit_item' => 'Edit Quote',
-        'new_item' => 'New Quote',
-        'all_items' => 'All Quotes',
-        'view_item' => 'View Quote',
-        'search_items' => 'Search Quotes',
-        'not_found' =>  'No Quotes Found',
-        'not_found_in_trash' => 'No Quotes found in Trash', 
+        'add_new' => __('Add New Quote'),
+        'add_new_item' => __('Add New Quote'),
+        'edit_item' => __('Edit Quote'),
+        'new_item' => __('New Quote'),
+        'all_items' => __('All Quotes'),
+        'view_item' => __('View Quote'),
+        'search_items' => __('Search Quotes'),
+        'not_found' =>  __('No Quotes Found'),
+        'not_found_in_trash' => __('No Quotes found in Trash'), 
         'parent_item_colon' => '',
-        'menu_name' => 'Quotes',
+        'menu_name' => __('Quotes'),
       ),
       'public' => true,
       'has_archive' => true,
@@ -29,18 +29,18 @@ function create_post_type() {
   );
 }
 
-function register_quote_type() {
+function qtd_register_quote_type() {
 // set up labels
   $labels = array(
-    'name'              => 'Quote Categories',
-    'singular_name'     => 'Quote Category',
-    'search_items'      => 'Search Quote Categories',
-    'all_items'         => 'All Quote Categories',
-    'edit_item'         => 'Edit Quote Category',
-    'update_item'       => 'Update Quote Category',
-    'add_new_item'      => 'Add New Quote Category',
-    'new_item_name'     => 'New Quote Category',
-    'menu_name'         => 'Quote Categories'
+    'name'              => __('Quote Categories'),
+    'singular_name'     => __('Quote Category'),
+    'search_items'      => __('Search Quote Categories'),
+    'all_items'         => __('All Quote Categories'),
+    'edit_item'         => __('Edit Quote Category'),
+    'update_item'       => __('Update Quote Category'),
+    'add_new_item'      => __('Add New Quote Category'),
+    'new_item_name'     => __('New Quote Category'),
+    'menu_name'         => __('Quote Categories')
   );
   // register taxonomy
   register_taxonomy( 'quotecat', 'quote_display', array(
@@ -51,20 +51,20 @@ function register_quote_type() {
   ) );
 
 }
-add_action( 'init', 'register_quote_type' );
+add_action( 'init', 'qtd_register_quote_type' );
 
-function register_quote_author() {
+function qtd_register_quote_author() {
 // set up labels
   $labels = array(
-    'name'              => 'Quote Authors',
-    'singular_name'     => 'Quote Author',
-    'search_items'      => 'Search Quote Authors',
-    'all_items'         => 'All Quote Authors',
-    'edit_item'         => 'Edit Quote Author',
-    'update_item'       => 'Update Quote Author',
-    'add_new_item'      => 'Add New Quote Author',
-    'new_item_name'     => 'New Quote Author',
-    'menu_name'         => 'Quote Author'
+    'name'              => __('Quote Authors'),
+    'singular_name'     => __('Quote Author'),
+    'search_items'      => __('Search Quote Authors'),
+    'all_items'         => __('All Quote Authors'),
+    'edit_item'         => __('Edit Quote Author'),
+    'update_item'       => __('Update Quote Author'),
+    'add_new_item'      => __('Add New Quote Author'),
+    'new_item_name'     => __('New Quote Author'),
+    'menu_name'         => __('Quote Author')
   );
   // register taxonomy
   register_taxonomy( 'quoteauth', 'quote_display', array(
@@ -75,4 +75,4 @@ function register_quote_author() {
   ) );
 
 }
-add_action( 'init', 'register_quote_author' );
+add_action( 'init', 'qtd_register_quote_author' );
